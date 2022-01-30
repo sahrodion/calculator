2eaa8531a0a471f78c4bd07c865ddd72e9b886fe
sahrodion <josiahosarodion@gmail.com>
2022-01-30 16:15:53 +0100

    added more functionality

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 function onLoad(){
+
+
     const display1El = document.querySelector('.display-1');
     const display2El = document.querySelector('.display-2');
-    const tempResultEl = document.querySelector('.temp-result');
+    const tempResultEl = document.querySelector('.temp-display');
     const operatiionEl = document.querySelectorAll('.operation');
     const numbersEl = document.querySelectorAll('.number');
     const equalEl = document.querySelector('.equal');
@@ -30,31 +32,66 @@ function onLoad(){
     });
 
 
-operatiionEl.forEach( operation => {
-    operation.addEventListener('click', (e) =>{
-        if (!disp2num) return;
+    operatiionEl.forEach( operation => {
+        operation.addEventListener('click', (e) =>{
+            if (!disp2num) return;
+            haveDot = false;
+            const operationName = e.target.innerText;
+            if (disp2num && disp1num && lastOperation){
+                mathOperation();
+            }else{
+                result = parseFloat(disp2num)
+            }
+            
+            clearVar(operationName);
+            lastOperation = operationName;
+            console.log(result);
+        })
+    });
+
+
+
+    equalEl.addEventListener('click', (e) => {
+        if (!disp1num && !disp2num) return;
         haveDot = false;
-        const operationName = e.target.innerText;
-        if (disp2num && disp1num && lastOperation){
-            mathOperation();
-        }else{
-            result = parseFloat(disp2num)
-        }
-        
-        clearVar(operationName);
-        lastOperation = operationName;
-        console.log(result);
+        mathOperation();
+        clearVar();
+        display2El.innerText = result;
+        display1El.innerText = '';
+        tempResultEl.innerText = '';
+        disp1num = '';
     })
-});
 
 
-function clearVar(name = ''){
-    disp1num += disp2num + ' ' + name + ' ';
-    display1El.innerText = disp1num;
-    display2El.innerText = '';
-    disp2num = ' ';
-    tempResultEl.innerText = result;
-}
+    clearEl.addEventListener('click', (e) =>{
+        display1El.innerText = '0';
+        display2El.innerText = '0';
+        tempResultEl.innerText = '0';
+    })
+
+
+    function clearVar(name = ''){
+        disp1num += disp2num + ' ' + name + ' ';
+        display1El.innerText = disp1num;
+        display2El.innerText = '';
+        disp2num = '';
+        tempResultEl.innerText = result;
+    }
+
+    function mathOperation(){
+        if(lastOperation === 'x'){
+            result = parseFloat(result) * parseFloat(disp2num);
+        }   else if(lastOperation === '+'){
+            result = parseFloat(result) + parseFloat(disp2num);
+        }   else if(lastOperation === '-'){
+            result = parseFloat(result) - parseFloat(disp2num);
+        }   else if(lastOperation === '%'){
+            result = parseFloat(result) % parseFloat(disp2num);
+        }   else if(lastOperation === '/'){
+            result = parseFloat(result) / parseFloat(disp2num);
+        }}
+    
+
 
 }
 
